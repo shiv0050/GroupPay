1bc9feb29f85e00c69e220433a521d95ecd26076
Pratham684 <pratham8641@gmail.com>
2024-07-29 12:40:01 +0530

    pom.xml fixed"

--- group-pay-backend/GroupPayMerchant/src/main/java/com/example/GroupPayMerchant/service/UserServiceImpl.java
+++ group-pay-backend/GroupPayMerchant/src/main/java/com/example/GroupPayMerchant/service/UserServiceImpl.java
@@ -2,16 +2,14 @@ package com.example.GroupPayMerchant.service;
 
 import com.example.GroupPayMerchant.models.requests.AddUserRequest;
 import com.example.GroupPayMerchant.models.responses.AddUserResponse;
-import com.example.GroupPay.utils.ConvertDate;
 import com.example.GroupPayMerchant.exceptions.InvalidCredentialException;
-import com.example.GroupPayMerchant.models.User;
 import com.example.GroupPayMerchant.repository.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.GroupPayMerchant.utils.JWTUtil;
+import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
